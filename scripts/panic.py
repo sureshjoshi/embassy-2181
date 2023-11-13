71ac405b9da16cf2fc8a3071707e5bc358b94ada
SJ <suresh@vicarasolutions.com>
2023-11-13 15:54:53 -0500

    Modified the script for two failure modes

--- scripts/panic.py
+++ scripts/panic.py
@@ -1,13 +1,29 @@
 import serial
+import time
 
 PORT = '/dev/ttyUSB1'
 BAUDRATE = 921600
 NUMBER_OF_BYTES = 2048
+FAST_PANIC = True
 
 def main():
     with serial.Serial(PORT, BAUDRATE, rtscts=True) as ser:
-        for i in range(0, NUMBER_OF_BYTES):
-            ser.write(bytes([i % 256]))
+        if FAST_PANIC:
+            print(f"Writing {NUMBER_OF_BYTES} bytes")
+            for i in range(0, NUMBER_OF_BYTES):
+                ser.write(bytes([i % 256]))
+
+        else: 
+            print("Writing 256 bytes")
+            for i in range(0, 256):
+                ser.write(bytes([i % 256]))
+
+            for i in range(0, NUMBER_OF_BYTES):
+                print(f"Sleep and write byte {i}")
+                time.sleep(1)
+                ser.write(bytes([i]))
+
+        
 
 if __name__ == "__main__":
     main()
